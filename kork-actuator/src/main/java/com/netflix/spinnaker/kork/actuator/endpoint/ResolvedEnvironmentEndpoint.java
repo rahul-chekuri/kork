f02293f019a63a3e2fc275664dc1bbb799a20cfe
rahul-chekuri <rahulgandhi.chekuri@opsmx.io>
2024-09-20 14:59:41 +0530

    fix: fixed compilation issues.

--- kork-actuator/src/main/java/com/netflix/spinnaker/kork/actuator/endpoint/ResolvedEnvironmentEndpoint.java
+++ kork-actuator/src/main/java/com/netflix/spinnaker/kork/actuator/endpoint/ResolvedEnvironmentEndpoint.java
@@ -18,31 +18,91 @@ package com.netflix.spinnaker.kork.actuator.endpoint;
 
 import static java.lang.String.format;
 
-import java.util.Arrays;
-import java.util.Map;
-import java.util.Optional;
-import java.util.SortedSet;
-import java.util.TreeSet;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.endpoint.SanitizableData;
 import org.springframework.boot.actuate.endpoint.Sanitizer;
+import org.springframework.boot.actuate.endpoint.SanitizingFunction;
 import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
 import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
 import org.springframework.core.env.*;
+import org.springframework.util.StringUtils;
 
 @Endpoint(id = "resolvedEnv")
 public class ResolvedEnvironmentEndpoint {
 
-  private final Sanitizer sanitizer = new Sanitizer();
+  private final Sanitizer sanitizer;
   private final Environment environment;
+  private static final String[] REGEX_PARTS = {"*", "$", "^", "+"};
+  private static final Set<String> URI_USERINFO_KEYS =
+      new LinkedHashSet<>(Arrays.asList("uri", "uris", "url", "urls", "address", "addresses"));
+
+  private static final Pattern URI_USERINFO_PATTERN =
+      Pattern.compile("^\\[?[A-Za-z][A-Za-z0-9+.\\-]+://.+:(.*)@.+$");
 
   @Autowired
   public ResolvedEnvironmentEndpoint(
       Environment environment, ResolvedEnvironmentConfigurationProperties properties) {
     this.environment = environment;
+    List<SanitizingFunction> sanitizingFunctions = new ArrayList<>();
     Optional.ofNullable(properties.getKeysToSanitize())
-        .map(p -> p.toArray(new String[0]))
-        .ifPresent(sanitizer::setKeysToSanitize);
+        .orElse(new ArrayList<>())
+        .forEach(
+            p ->
+                sanitizingFunctions.add(
+                    (data) -> {
+                      Pattern pattern = getPattern(p);
+                      if (pattern.matcher(data.getKey()).matches()) {
+                        if (keyIsUriWithUserInfo(pattern)) {
+                          return data.withValue(sanitizeUris(data.getValue().toString()));
+                        }
+                        return data.withValue(SanitizableData.SANITIZED_VALUE);
+                      }
+                      return data;
+                    }));
+    sanitizer = new Sanitizer(sanitizingFunctions);
+  }
+
+  private Pattern getPattern(String value) {
+    if (isRegex(value)) {
+      return Pattern.compile(value, Pattern.CASE_INSENSITIVE);
+    }
+    return Pattern.compile(".*" + value + "$", Pattern.CASE_INSENSITIVE);
+  }
+
+  private boolean isRegex(String value) {
+    for (String part : REGEX_PARTS) {
+      if (value.contains(part)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private boolean keyIsUriWithUserInfo(Pattern pattern) {
+    for (String uriKey : URI_USERINFO_KEYS) {
+      if (pattern.matcher(uriKey).matches()) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private Object sanitizeUris(String value) {
+    return Arrays.stream(value.split(",")).map(this::sanitizeUri).collect(Collectors.joining(","));
+  }
+
+  private String sanitizeUri(String value) {
+    Matcher matcher = URI_USERINFO_PATTERN.matcher(value);
+    String password = matcher.matches() ? matcher.group(1) : null;
+    if (password != null) {
+      return StringUtils.replace(
+          value, ":" + password + "@", ":" + SanitizableData.SANITIZED_VALUE + "@");
+    }
+    return value;
   }
 
   @ReadOperation
@@ -53,7 +113,9 @@ public class ResolvedEnvironmentEndpoint {
                 property -> property,
                 property -> {
                   try {
-                    return sanitizer.sanitize(property, environment.getProperty(property));
+                    return sanitizer.sanitize(
+                        new SanitizableData(null, property, environment.getProperty(property)),
+                        true);
                   } catch (Exception e) {
                     return format("Exception occurred: %s", e.getMessage());
                   }
--- kork-eureka/src/main/java/com/netflix/spinnaker/kork/eureka/EurekaAutoConfiguration.java
+++ kork-eureka/src/main/java/com/netflix/spinnaker/kork/eureka/EurekaAutoConfiguration.java
@@ -17,10 +17,9 @@
 package com.netflix.spinnaker.kork.eureka;
 
 import com.netflix.appinfo.*;
-import com.netflix.discovery.DefaultEurekaClientConfig;
-import com.netflix.discovery.DiscoveryClient;
-import com.netflix.discovery.EurekaClient;
-import com.netflix.discovery.EurekaClientConfig;
+import com.netflix.discovery.*;
+import com.netflix.discovery.shared.transport.jersey.TransportClientFactories;
+import com.netflix.discovery.shared.transport.jersey3.Jersey3TransportClientFactories;
 import com.netflix.eventbus.impl.EventBusImpl;
 import com.netflix.eventbus.spi.EventBus;
 import com.netflix.spinnaker.kork.discovery.DiscoveryAutoConfiguration;
@@ -46,14 +45,17 @@ public class EurekaAutoConfiguration {
     return new EventBusImpl();
   }
 
-  /** @deprecated use EurekaClient rather than DiscoveryClient */
   @Bean
-  @Deprecated
   public DiscoveryClient discoveryClient(
       ApplicationInfoManager applicationInfoManager,
-      EurekaClientConfig eurekaClientConfig,
-      DiscoveryClient.DiscoveryClientOptionalArgs optionalArgs) {
-    return new DiscoveryClient(applicationInfoManager, eurekaClientConfig, optionalArgs);
+      EurekaClientConfig config,
+      TransportClientFactories transportClientFactories) {
+    return new DiscoveryClient(applicationInfoManager, config, transportClientFactories);
+  }
+
+  @Bean
+  public TransportClientFactories transportClientFactories() {
+    return Jersey3TransportClientFactories.getInstance();
   }
 
   @Bean
@@ -83,16 +85,6 @@ public class EurekaAutoConfiguration {
   EurekaClientConfig eurekaClientConfig(
       EurekaConfigurationProperties eurekaConfigurationProperties) {
     return new DefaultEurekaClientConfig(eurekaConfigurationProperties.getClient().getNamespace());
-  }
-
-  @Bean
-  DiscoveryClient.DiscoveryClientOptionalArgs optionalArgs(
-      EventBus eventBus, HealthCheckHandler healthCheckHandler) {
-    DiscoveryClient.DiscoveryClientOptionalArgs args =
-        new DiscoveryClient.DiscoveryClientOptionalArgs();
-    args.setEventBus(eventBus);
-    args.setHealthCheckHandlerProvider(new StaticProvider<>(healthCheckHandler));
-    return args;
   }
 
   @Bean
