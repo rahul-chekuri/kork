5133068e2b02d72704c5486ddacf61725e345a12
rahul-chekuri <rahulgandhi.chekuri@opsmx.io>
2024-10-01 09:36:49 +0530

    fix: refactored code.

--- kork-actuator/src/main/java/com/netflix/spinnaker/kork/actuator/endpoint/ResolvedEnvironmentEndpoint.java
+++ kork-actuator/src/main/java/com/netflix/spinnaker/kork/actuator/endpoint/ResolvedEnvironmentEndpoint.java
@@ -18,8 +18,8 @@ package com.netflix.spinnaker.kork.actuator.endpoint;
 
 import static java.lang.String.format;
 
+import com.netflix.spinnaker.kork.actuator.utils.SanitizerUtils;
 import java.util.*;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,19 +29,11 @@ import org.springframework.boot.actuate.endpoint.SanitizingFunction;
 import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
 import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
 import org.springframework.core.env.*;
-import org.springframework.util.StringUtils;
 
 @Endpoint(id = "resolvedEnv")
 public class ResolvedEnvironmentEndpoint {
-
   private final Sanitizer sanitizer;
   private final Environment environment;
-  private static final String[] REGEX_PARTS = {"*", "$", "^", "+"};
-  private static final Set<String> URI_USERINFO_KEYS =
-      new LinkedHashSet<>(Arrays.asList("uri", "uris", "url", "urls", "address", "addresses"));
-
-  private static final Pattern URI_USERINFO_PATTERN =
-      Pattern.compile("^\\[?[A-Za-z][A-Za-z0-9+.\\-]+://.+:(.*)@.+$");
 
   @Autowired
   public ResolvedEnvironmentEndpoint(
@@ -54,55 +46,17 @@ public class ResolvedEnvironmentEndpoint {
             p ->
                 sanitizingFunctions.add(
                     (data) -> {
-                      Pattern pattern = getPattern(p);
+                      Pattern pattern = SanitizerUtils.getPattern(p);
                       if (pattern.matcher(data.getKey()).matches()) {
-                        if (keyIsUriWithUserInfo(pattern)) {
-                          return data.withValue(sanitizeUris(data.getValue().toString()));
+                        if (SanitizerUtils.keyIsUriWithUserInfo(pattern)) {
+                          return data.withValue(
+                              SanitizerUtils.sanitizeUris(data.getValue().toString()));
                         }
                         return data.withValue(SanitizableData.SANITIZED_VALUE);
                       }
                       return data;
                     }));
     sanitizer = new Sanitizer(sanitizingFunctions);
-  }
-
-  private Pattern getPattern(String value) {
-    if (isRegex(value)) {
-      return Pattern.compile(value, Pattern.CASE_INSENSITIVE);
-    }
-    return Pattern.compile(".*" + value + "$", Pattern.CASE_INSENSITIVE);
-  }
-
-  private boolean isRegex(String value) {
-    for (String part : REGEX_PARTS) {
-      if (value.contains(part)) {
-        return true;
-      }
-    }
-    return false;
-  }
-
-  private boolean keyIsUriWithUserInfo(Pattern pattern) {
-    for (String uriKey : URI_USERINFO_KEYS) {
-      if (pattern.matcher(uriKey).matches()) {
-        return true;
-      }
-    }
-    return false;
-  }
-
-  private Object sanitizeUris(String value) {
-    return Arrays.stream(value.split(",")).map(this::sanitizeUri).collect(Collectors.joining(","));
-  }
-
-  private String sanitizeUri(String value) {
-    Matcher matcher = URI_USERINFO_PATTERN.matcher(value);
-    String password = matcher.matches() ? matcher.group(1) : null;
-    if (password != null) {
-      return StringUtils.replace(
-          value, ":" + password + "@", ":" + SanitizableData.SANITIZED_VALUE + "@");
-    }
-    return value;
   }
 
   @ReadOperation
